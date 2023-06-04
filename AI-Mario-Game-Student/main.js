a4b91ffec223456dcd014c71794903ec828290a2
Simon-2022 <96212730+Simon-2022@users.noreply.github.com>
2023-06-04 19:08:04 +1000

    Add files via upload

--- AI-Mario-Game-Student/main.js
+++ AI-Mario-Game-Student/main.js
@@ -3,6 +3,11 @@ noseY = "";
 gameStatus = "";
 function preload() {
 	world_start = loadSound("world_start.wav");
+	mario_jump = loadSound("jump.wav");
+	mario_coin = loadSound("coin.wav");
+	mario_gameOver = loadSound("gameover.wav");
+	mario_die = loadSound("mariodie.wav")
+	mario_kick = loadSound("kick.wav");
 	setSprites();
 	MarioAnimation();
 }
